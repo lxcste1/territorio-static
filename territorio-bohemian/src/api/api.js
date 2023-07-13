a3b624a37988eb5937c6883b9944016502520ad6
tellol <lucas@tobs.com.ar>
2023-07-13 00:27:10 -0300

    Resuelvo errores que aparecieron al hacer el build

--- territorio-bohemian/src/api/api.js
+++ territorio-bohemian/src/api/api.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-export default function api() {
+export default function Api() {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
